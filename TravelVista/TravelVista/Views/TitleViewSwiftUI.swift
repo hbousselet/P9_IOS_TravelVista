4b16fb0c801006d022aa42a22b30e32d7aacf3ad
Hugues <hugues.bousselet@gmail.com>
2025-04-03 10:50:43 +0200

    Create the SwiftUI view TitleViewSwiftUI and its components.

---
+++ TravelVista/TravelVista/Views/TitleViewSwiftUI.swift
@@ -0,0 +1,52 @@
+//
+//  TitleViewSwiftUI.swift
+//  TravelVista
+//
+//  Created by Hugues BOUSSELET on 03/04/2025.
+//
+
+import SwiftUI
+
+struct TitleViewSwiftUI: View {
+    var viewModel: Country
+    var body: some View {
+        HStack(alignment: .center) {
+            NamesViewSwiftUI(countryName: viewModel.name, capitalName: viewModel.capital)
+            Spacer()
+            RateViewSwiftUI(rate: viewModel.rate)
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 8)
+    }
+}
+
+struct NamesViewSwiftUI: View {
+    let countryName: String
+    let capitalName: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(countryName)
+                .font(.system(size: 22, weight: .medium))
+                .foregroundStyle(.customBlue)
+            Text(capitalName)
+                .font(.system(size: 17))
+                .foregroundStyle(Color(UIColor.systemGray))
+        }
+    }
+}
+
+struct RateViewSwiftUI: View {
+    let rate: Int
+    var body: some View {
+        ForEach(0..<rate) { _ in
+            Image(systemName: "star.fill")
+                .foregroundColor(.yellow)
+        }
+    }
+    
+}
+
+#Preview {
+    TitleViewSwiftUI(viewModel: Country(name: "France", capital: "Paris", description: "COucouc", rate: 4, pictureName: "Bla", coordinates: Coordinates(latitude: 123, longitude: 13)))
+}

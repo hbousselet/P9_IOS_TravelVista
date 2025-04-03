baa6bf352176720180ed3f81b6b8aa2b3ec9fd37
Hugues <hugues.bousselet@gmail.com>
2025-04-03 14:15:32 +0200

    Create a new View in SwiftUI for the ListView.

--- TravelVista/TravelVista/Views/ListViewSwiftUI.swift
+++ TravelVista/TravelVista/Views/ListViewSwiftUI.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ListViewSwiftUI: View {
+    let regions: [Region]
+    @State private var singleSelection: UUID?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(selection: $singleSelection) {
+            ForEach(regions) { region in
+                Section(header: Text(region.name)) {
+                    ForEach(region.countries) { country in
+                        HStack {
+                            Image(country.pictureName)
+                                .resizable()
+                                .frame(width: 52, height: 52)
+                                .scaledToFit()
+                                .clipShape(Circle())
+                            VStack(alignment: .leading) {
+                                Text(country.name)
+                                    .font(.system(size: 20))
+                                    .foregroundStyle(.customBlue)
+                                Text(country.capital)
+                                    .font(.system(size: 15))
+                            }
+                            Spacer()
+                            HStack {
+                                Text(String(country.rate))
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(.customSand)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ListViewSwiftUI()
+    ListViewSwiftUI(regions: [Region(name: "Europe", countries: [Country(name: "France", capital: "Paris", description: "Blabdhjhsbjqhsxbjsqbxjsbsxh", rate: 4, pictureName: "canada", coordinates: Coordinates(latitude: 12, longitude: 134)), Country(name: "Italia", capital: "Roma", description: "Blabdhjhsbjqhsxbjsqbxjsbsxh", rate: 4, pictureName: "roma", coordinates: Coordinates(latitude: 12, longitude: 134))]), Region(name: "Asia", countries: [Country(name: "China", capital: "Beijing", description: "Blabdhjhsbjqhsxbjsqbxjsbsxh", rate: 4, pictureName: "Beijing", coordinates: Coordinates(latitude: 12, longitude: 134)), Country(name: "South Korea", capital: "Seoul", description: "Blabdhjhsbjqhsxbjsqbxjsbsxh", rate: 4, pictureName: "Seoul", coordinates: Coordinates(latitude: 12, longitude: 134))]), Region(name: "America", countries: [Country(name: "USA", capital: "Washington", description: "Blabdhjhsbjqhsxbjsqbxjsbsxh", rate: 4, pictureName: "Washington", coordinates: Coordinates(latitude: 12, longitude: 134)), Country(name: "Canada", capital: "Ottawa", description: "Blabdhjhsbjqhsxbjsqbxjsbsxh", rate: 4, pictureName: "canada", coordinates: Coordinates(latitude: 12, longitude: 134))]),])
 }
